1f58bfa97d8b3b2966e35adef72218e74bd62439
Modaino <Modaino@users.noreply.github.com>
2021-09-24 14:12:48 -0400

    Add files via upload

--- pSAT_problem.py
+++ pSAT_problem.py
@@ -65,23 +65,44 @@ class SATProblem:
             return True
             #print('The solution "'+ sol_file_name +'" to '+ assignment +' is correct')
 
-class RK5:
-    def __init__(self, dim, f, df = None, y_init = None, Nmax = 10000) -> None:
+class Integrator:
+    def __init__(self, dim, f, y_init = None, Nmax = 10000, h = 0.0025) -> None:
         if not dim or not f:
             raise ValueError
-        
+        self.f = f
+        self.dim = dim
+        self.h = h
+        self.y = np.zeros(dim)
         if not y_init:
             self.y_init = np.random.randn(*(dim, 1))
         else:
             self.y_init = y_init
 
-    def step(f, df, h, y_n):
-        k1 = h * f(y_n)
-        k2 = h * f(y_n + k1*1/3 + h * np.dot(df(y_n), k1))
-        k3 = h * f(y_n + k1*152/125 + k2*252/125 - h * 44/125 * np.dot(df(y_n), k1))
-        k4 = h * f(y_n + k1*19/2 - k2*72/7 + k3*25/14 + h * 5/2 * np.dot(df(y_n), k1))
-        return y_n + 5/48*k1 + 27/56*k2 + 125/336*k3 + 1/24*k4
+class RK5( Integrator ):
+    def __init__(self, dim, f, df = None, y_init = None, Nmax = 10000, h = 0.0025) -> None:
+        if df:
+            raise ValueError
+        Integrator.__init__(self, dim, f, y_init, Nmax, h)
+
+    def step(self, f, df):
+        k1 = self.h * f(self.y)
+        k2 = self.h * f(self.y + k1*1/3 + self.h * np.dot(df(self.y), k1))
+        k3 = self.h * f(self.y + k1*152/125 + k2*252/125 - self.h * 44/125 * np.dot(df(self.y), k1))
+        k4 = self.h * f(self.y + k1*19/2 - k2*72/7 + k3*25/14 + self.h * 5/2 * np.dot(df(self.y), k1))
+        return self.y + 5/48*k1 + 27/56*k2 + 125/336*k3 + 1/24*k4
+
+class RK4( Integrator ):
+    def __init__(self, dim, f, y_init = None, Nmax = 10000, h = 0.0025) -> None:
+        Integrator.__init__(self, dim, f, y_init, Nmax, h)
+
+    def step(self):
+        k1 = self.h*self.f(self.y)
+        k2 = self.h*self.f(self.y+0.5*k1)
+        k3 = self.h*self.f(self.y+0.5*k2)
+        k4 = self.h*self.f(self.y+k3)
+        return self.y + (k1 + 2 * k2 + 2 * k3 + k4) / 6.0 
 
 class CTDSolver:
-    def __init__(self, SATproblem) -> None:
+    def __init__(self, SATproblem, solver) -> None:
         self.SATproblem = SATproblem
+        self.solver = solver
